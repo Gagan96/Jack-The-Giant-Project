ae43c9687b3d218186de9e1d030034278bdd180a
Gagam96 <gagan.96@hotmail.com>
2018-03-05 20:59:08 +0100

    Initial commit

---
+++ core/src/helpers/GameInfo.java
@@ -0,0 +1,14 @@
+package helpers;
+
+/**
+ * Created by iam9091107 on 3/5/18.
+ */
+
+public class GameInfo {
+
+    public static final int WIDTH = 480;
+    public static final int HEIGHT = 800;
+    public static final int PPM = 100;
+
+
+}
